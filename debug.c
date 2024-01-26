8b6fbedeb1a41b21f3e949c93dc1326447bfe0a5
Allison Blair <blair.al@northeastern.edu>
2024-01-25 22:02:47 -0500

    hw 1

--- debug.c
+++ debug.c
@@ -5,22 +5,17 @@
 #include <string.h>
 #include <ctype.h>
 
-// Exercise 5: this function is supposed to return a value that doubles "i"
-int double_num(int i) {
-    return 2*i;
+
+void change(char *source) {
+          source[0] = 'a';
+          printf("%s\n", source);
 }
 
+int main() {
 
-int main(void)
-{
-    // Exercise 5: fix a syntax error
-    int num = 10;
-    int doub_num = double_num(num);
-    printf("double a number (%d) is (%d)\n", num, doub_num);
-    // check if the returned value is what we want
-    assert(num + num == doub_num);
-    printf("Pass!\n");
+char *a = "ABC";
+change(a);
 
-    return 0;
-}
+return 0;
+} 
 
